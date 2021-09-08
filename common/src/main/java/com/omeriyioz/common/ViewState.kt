96d7b7bdf98560e3b718bf5ecc3f7ef0571f2d74
OMERIYIOZ <omer.iyioz@turkcell.com.tr>
2021-09-08 09:11:27 +0300

    improve

--- common/src/main/java/com/omeriyioz/common/ViewState.kt
+++
@@ -1,9 +0,0 @@
-package com.omeriyioz.common
-
-sealed class ViewState<out T : Any> {
-
-    data class Success<out T : Any>(val result: T) : ViewState<T>()
-    data class Error(val exception: Exception) : ViewState<Nothing>()
-    object Loading : ViewState<Nothing>()
-
-}
